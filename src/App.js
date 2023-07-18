2a831b501884ce3729c0ab07d0c2407e30f9c2e2
EstherUduma <estheruduma23@gmail.com>
2023-07-18 14:20:38 +0100

    Using App.css and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import Homepage from './Homepage';
-import AboutMe from './AboutMe';
-import { Routes, Route, Link } from 'react-router-dom';
+import "./App.css";
+import Homepage from "./Homepage";
+import AboutMe from "./AboutMe";
+import Contact from "./Contact";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <nav className="nav">
-        <Link to="/" className="nav-item">Homepage</Link>
-        <Link to="/about-me" className="nav-item">About Me</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about-me" element={<AboutMe />} />
+    <div> 
+	  <nav>
+      <Link to="/" className="nav-item">Homepage</Link>
+      <Link to="/about-me" className="nav-item">About Me</Link>
+      <Link to="/contact" className="nav-item">Contact</Link>
+	  </nav>
+      <Routes> 
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about-me" element={<AboutMe />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
