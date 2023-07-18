cf31d5cbfd44e2e4716c8e022554ca5e5e6095c3
EstherUduma <estheruduma23@gmail.com>
2023-07-18 12:46:13 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Homepage from './Homepage';
+import AboutMe from './AboutMe';
 
 function App() {
   return (
     <div>
       <h1>Testing</h1>
+      <Homepage />
+      <AboutMe />
     </div>
   );
 }
